e9a68bec03449b25b7503c962ced5622960272f8
danielgiron <73446404+danielgiron@users.noreply.github.com>
2022-12-02 00:27:56 -0500

    Finalized MacrOS Presentation

--- src/Slides.js
+++ src/Slides.js
@@ -6,13 +6,49 @@ import Tasky5 from "./media/examples/Tasky5.gif";
 import Tasky6 from "./media/examples/Tasky6.png";
 import Tasky7 from "./media/examples/Tasky7.png";
 
-import Macros1 from "./media/examples/Macros1.png";
-import Macros2 from "./media/examples/Macros2.png";
-import Macros3 from "./media/examples/Macros3.png";
+import Macros1 from "./media/examples/Macros1.gif";
+import Macros2 from "./media/examples/Macros2.gif";
+import Macros3 from "./media/examples/Macros3.gif";
 import Macros4 from "./media/examples/Macros4.png";
 import Macros5 from "./media/examples/Macros5.png";
 import Macros6 from "./media/examples/Macros6.png";
-import Macros7 from "./media/examples/Macros7.png";
+
+export const macrosSlides = [
+  {
+    header: "Nutritional Info Lookup",
+    image: Macros1,
+    caption:
+      "(Click images expand) Lookup nutritional info for your favorite foods and add them to your collection",
+  },
+  {
+    header: "Recipes",
+    image: Macros2,
+    caption:
+      "Create recipes using your saved items and view their nutritional composition",
+  },
+  {
+    header: "Tracker (Dashboard)",
+    image: Macros3,
+    caption:
+      "Add items to your daily food logs, and revisit previous entries and info",
+  },
+  {
+    header: "Mobile Friendly",
+    image: Macros4,
+    caption:
+      "All the functionality of a web application presented in a familiar mobile layout",
+  },
+  {
+    header: "Responsive Design",
+    image: Macros5,
+    caption: "Every feature designed responsively",
+  },
+  {
+    header: "Calculator Tools",
+    image: Macros6,
+    caption: "Added built-in TDEE and Macro Calculators for easy planning",
+  },
+];
 
 export const taskySlides = [
   {
@@ -52,44 +88,5 @@ export const taskySlides = [
     image: Tasky4,
     caption:
       "Easy to use and understand interface, like the state tracking settings page above",
-  },
-];
-
-export const macrosSlides = [
-  {
-    header: "Front Page",
-    image: Macros1,
-    caption: "Front page of Tasky Social Network",
-  },
-  {
-    header: "Profile Page",
-    image: Macros3,
-    caption:
-      "Each user has their own profile page from which contacts can message or send tasks from",
-  },
-  {
-    header: "Chat",
-    image: Macros4,
-    caption: "Tasky users are able to have live chats with other users",
-  },
-  {
-    header: "Tasks Page",
-    image: Macros2,
-    caption: "Main page of Task displaying all of user's current tasks",
-  },
-  {
-    header: "Mobile Chat",
-    image: Macros5,
-    caption: "The chat component looks great on mobile displays!",
-  },
-  {
-    header: "Chats",
-    image: Macros6,
-    caption: "A list of user's active chats ",
-  },
-  {
-    header: "Fully Responsive",
-    image: Macros7,
-    caption: "The entire app has been mobile responsive!",
   },
 ];
